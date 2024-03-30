1a5977a1225bc2ecce06c1f6190526a23c42ea5e
soheil khaledabadi <soheilkhaledabdi@gmail.com>
2024-03-31 00:39:08 +0330

    Update -> change frontend

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>استعلام حجم تیز پینگ</title>
+    <title>استعلام حجم نیک گیم</title>
     <link rel="stylesheet" href="style/output.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://rsms.me/">
@@ -12,7 +12,7 @@
 </head>
 @livewireStyles
 
-<body dir="rtl">
+<body dir="rtl" class="bg-[#F2F3F3]">
     {{ $slot }}
 </body>
 @livewireScripts
